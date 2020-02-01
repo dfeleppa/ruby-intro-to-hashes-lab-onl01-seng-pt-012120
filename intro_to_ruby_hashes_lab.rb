6f11760c1fc363b69330df1017b444eda08a4047
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-01 23:53:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,6 +11,7 @@ monopoly =  {
   :railroads=>{}
 }
 end
+def monopoly_with_third_tier
 monopoly =  {
   :railroads=>
   {
@@ -20,29 +21,18 @@ monopoly =  {
       :one_piece_owned=>25,
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
-      :four_pieces_owned=>200
-    },
+      :four_pieces_owned=>200,
     :names=>
     {
-      :reading_railroad=>
-      {
-        "mortgage_value"=>"$100"
-      },
-      :pennsylvania_railroad=>
-      {
-        "mortgage_value"=>"$200"
-      },
-      :b_and_o_railroad=>
-      {
-        "mortgage_value"=>"$400"
-      },
+      :reading_railroad=>,
+      :pennsylvania_railroad=>,
+      :b_and_o_railroad=>,
       :shortline=>
-      {
-        "mortgage_value"=>"$800"
-      }
     }
   }
 }
+}
+
 end
 
 def monopoly_with_fourth_tier
